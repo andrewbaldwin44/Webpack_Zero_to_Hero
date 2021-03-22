fe95608acfdc0f1917926bf66018ab2793b8da64
Andrew Baldwin <abald.cg.ide@aldogroup.com>
2021-03-22 10:49:37 -0400

    Webpack image and font loaders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 
 import { print } from "./print";
+import Penguin from "./assets/images/penguin.jpg";
 import "./styles/style.css";
 import "./styles/style.scss";
 
@@ -8,14 +9,18 @@ function component() {
   const element = document.createElement("div");
   const cssSpan = document.createElement("span");
   const scssSpan = document.createElement("span");
+  const myPenguin = new Image();
 
   cssSpan.innerHTML = _.join(["Hello", "css"], " ");
   cssSpan.classList.add("hello-css");
   scssSpan.innerHTML = _.join(["Hello", "sass"], " ");
   scssSpan.classList.add("hello-sass");
 
+  myPenguin.src = Penguin;
+
   element.appendChild(cssSpan);
   element.appendChild(scssSpan);
+  element.appendChild(myPenguin);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,15 @@ module.exports = {
 
         use: ["style-loader", "css-loader", "sass-loader"]
       },
-      { test: /\.ts$/, use: "ts-loader" }
+      { test: /\.ts$/, use: "ts-loader" },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource"
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource"
+      }
     ]
   },
 
